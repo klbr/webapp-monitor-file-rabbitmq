c282876b0dabed86abb91f35452dcdcda21643b8
Kleber Bernardo <kleberbernardo@outlook.com>
2019-06-17 04:34:55 -0300

    console upload

---
+++ Desafio.ConsoleUpload/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Desafio.ConsoleUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!args.Any())
+            {
+                Console.WriteLine("Path argument is missing");
+                Console.ReadLine();
+                return;
+            }
+
+            foreach (var path in args)
+            {
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine($"File '{path}' not exists. Skipping...");
+                    continue;
+                }
+
+                var data = File.ReadAllBytes(path);
+                var filename = path.Split('\\').LastOrDefault();
+
+                if (data.Length == 0 || string.IsNullOrEmpty(filename))
+                {
+                    Console.WriteLine($"File '{path}' is invalid. Skipping...");
+                    continue;
+                }
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:5001");
+
+                    var bytes = new ByteArrayContent(data);
+                    bytes.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                    {
+                        Name = "\"file\"",
+                        FileName = $"\"{filename}\"",
+                        Size = data.LongLength,
+                    };
+                    bytes.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                    var multiContent = new MultipartFormDataContent
+                    {
+                        { bytes }
+                    };
+                    using (var result = client.PostAsync("api/process", multiContent).Result)
+                    {
+                        if (result.StatusCode != HttpStatusCode.OK)
+                        {
+                            throw new Exception($"Erro when use Api. StatusCode: {result.StatusCode}");
+                        }
+                    }
+                }
+                Console.WriteLine($"File '{filename}' was uploaded successfully");
+            }
+        }
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,8 +1,37 @@
-* Add Migrations:
-dotnet ef migrations add Initial -s WebAppDesafio -p DataDesafio -v
+## Setup
 
-* Update Migrations:
-dotnet ef database update -s WebAppDesafio -p DataDesafio -v
+### EF Core
+- Update Migrations:
+    - ```$ dotnet ef database update -s WebAppDesafio -p DataDesafio```
 
-* RabbitMQ:
-https://www.rabbitmq.com/install-windows.html
+### RabbitMQ:
+- Install Erlang from: [Erlang.org](https://www.erlang.org/downloads)
+- Install RabbitMQ from: [RabbitMQ.com](https://www.rabbitmq.com/download.html)
+- Enable Management (PowerShell):
+    - ```C:\Program Files\RabbitMQ Server\rabbitmq_server-3.7.15\sbin> ./rabbitmq-plugins.bat enable rabbitmq_management```
+    - http://localhost:15672
+    - user: guest
+    - password: guest
+
+### SQLServer
+ - Install Developer: [Microsoft.com/Sql-Server](https://www.microsoft.com/pt-br/sql-server/sql-server-downloads)
+
+
+## Running
+
+- Open solution with Visual Studio 2019
+
+### Web Application: *Desafio.WebApp*
+- Run on 5000/5001 ports
+- https://localhost:5001 (UploadFile)
+- https://localhost:5001/Report (Reports)
+- https://localhost:5001/Report/Details?id=[id] (Report For File)
+- Producer for Queue
+
+### Background Application: *Desafio.Jobs*
+- Producer/Consumer for queue
+- Limit of 4 request/min to VirusTotal API
+  - Each request within 15 seconds
+
+### Console Application for Upload: *Desafio.ConsoleUpload*
+- Receives list of file paths as argument and send to WebApi
